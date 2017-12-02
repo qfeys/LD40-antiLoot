bab2df49536776a7a919349e7f13a42b645390dc
qfeys <qfeys@skynet.be>
2017-12-02 10:07:11 +0100

    Inventory screen added

--- Assets/Scripts/UI_Stats.cs
+++ Assets/Scripts/UI_Stats.cs
@@ -11,8 +11,16 @@ public class UI_Stats : MonoBehaviour {
 	void Start ()
     {
         QuintensUITools.Graphics.SetDefaultFont(DefaultFont);
+        QuintensUITools.Graphics.SetPath(@"Assets\");
+        QuintensUITools.Graphics.LoadGraphics();
+        UI_inventory.Create(transform.parent.gameObject);
 
+        CreateMe();
 
+    }
+
+    private void CreateMe()
+    {
         TextBox xpBox1 = new TextBox(transform, "Xp", 36);
         xpBox1.transform.anchoredPosition = new Vector2(5, 38);
         TextBox xpBox2 = new TextBox(transform, TextRef.Create(() => Player.instance.xp), 36, TextAnchor.MiddleRight);
@@ -25,11 +33,13 @@ public class UI_Stats : MonoBehaviour {
         weightBox1.transform.anchoredPosition = new Vector2(5, -38);
         TextBox weightBox2 = new TextBox(transform, "###", 36, TextAnchor.MiddleRight);
         weightBox2.transform.anchoredPosition = new Vector2(-5, -38);
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("inventory"))
+        {
+            UI_inventory.go.SetActive(!UI_inventory.go.activeSelf);
+        }
 	}
 }
---
+++ Assets/Scripts/UI_inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using QuintensUITools;
+
+static class UI_inventory
+{
+    static public GameObject go;
+
+    static public void Create(GameObject canvas)
+    {
+        go = new GameObject("Inspector", typeof(RectTransform));
+        go.transform.SetParent(canvas.transform);
+        RectTransform tr = (RectTransform)go.transform;
+        tr.sizeDelta = new Vector2(400, 400);
+        tr.anchorMin = new Vector2(.5f, .5f);
+        tr.anchorMax = new Vector2(.5f, .5f);
+        tr.pivot = new Vector2(.5f, .5f);
+        tr.anchoredPosition = new Vector2(0, 0);
+        Image im = go.AddComponent<Image>();
+        im.sprite = QuintensUITools.Graphics.GetSprite("Inventory_window");
+        im.type = Image.Type.Filled;
+
+        InfoTable inv = InfoTable.Create(go.transform, () => Player.instance.inventory, (Loot l) => new List<TextRef>() { TextRef.Create(() => l) },
+            200, new List<TextRef>() { "Inventory" }, 24);
+        inv.transform.anchorMin = new Vector2(1, 1);
+        inv.transform.anchorMax = new Vector2(1, 1);
+        inv.transform.pivot = new Vector2(1, 1);
+        inv.transform.anchoredPosition = new Vector2(-10, -50);
+
+        go.SetActive(false);
+    }
+
+}
