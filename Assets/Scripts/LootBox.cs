e41bd54d0f5016f92c1985aa1efcb2748a3cf959
qfeys <qfeys@skynet.be>
2017-12-02 06:12:25 +0100

    LootBox

---
+++ Assets/Scripts/LootBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBox : MonoBehaviour {
+
+    Vector2 pos;
+    float lifetime;
+    float rps = .5f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = (Vector3)pos + Quaternion.Euler(0, 0, lifetime * 360 * rps) * Vector3.up * .1f;
+        lifetime += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/LootManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour {
+
+    public GameObject LootBoxPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaceLootbox(Vector2 pos)
+    {
+        GameObject lb = GameObject.Instantiate(LootBoxPrefab, pos, Quaternion.identity);
+
+    }
+}
