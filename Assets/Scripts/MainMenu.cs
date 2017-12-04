4fb83082f3d22de940ebe736ab91d2c7261b48da
qfeys <qfeys@skynet.be>
2017-12-04 01:58:31 +0100

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("_SCENE");
+    }
+}
